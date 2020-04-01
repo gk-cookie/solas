1fb0fc7991479da3a887a86e3272b33f48324e92
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-01 17:15:44 +0100

    Completed the scope video and have examples

--- mod2-vids5-10.js
+++ mod2-vids5-10.js
@@ -9,21 +9,37 @@ let a = sayHello;
 a();
 
 function sayHello(name) {
-    console.log("--------------");
-    console.log("Hello" + name + '!' );
-    console.log("--------------");
-  }
+  console.log("--------------");
+  console.log("Hello" + name + "!");
+  console.log("--------------");
+}
 
-  sayHello('Jim');
-  sayHello('Joe');
-  sayHello('Tom');
+sayHello("Jim");
+sayHello("Joe");
+sayHello("Tom");
 
-  function calculateTax(amount) {
-      let result = amount * 1.21;
-      return result;
-  }
+function calculateTax(amount) {
+  let result = amount * 1.21;
+  return result;
+}
 
-  let afterTax = calculateTax(78);
-  console.log(afterTax);
-  
-  
+let afterTax = calculateTax(78);
+console.log(afterTax);
+
+let b = "first";
+
+// Scope
+
+function scopeTest() {
+  b = `changed value`;
+  let c = "second";
+  if (b != "") {
+      console.log('inside if' + b);
+    }
+}
+// console.log(c); Error c is not defined, it is only inside the function
+
+console.log(b);
+scopeTest();
+
+
