a4e10f74d34ba586703e3c85ab810243faa6eb31
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-01 17:57:14 +0100

    Object Video watched and examples

--- mod2-vids5-10.js
+++ mod2-vids5-10.js
@@ -34,12 +34,59 @@ function scopeTest() {
   b = `changed value`;
   let c = "second";
   if (b != "") {
-      console.log('inside if' + b);
-    }
+    console.log("inside if" + b);
+  }
 }
 // console.log(c); Error c is not defined, it is only inside the function
 
 console.log(b);
 scopeTest();
 
+// Object Leterals
 
+let car = {
+  make: "bmw",
+  model: "745",
+  year: 2015,
+  getPrice: function() {
+    return 15000;
+  },
+  printDescription: function() {
+    console.log(this.make + " " + this.model);
+  }
+};
+
+car.printDescription();
+console.log(car.year);
+
+console.log(car["year"]);
+
+let anotherCar = {};
+anotherCar.whatever = "bob";
+console.log(anotherCar.whatever);
+
+let house = {
+  myProperty: { b: "hi" }
+};
+
+console.log(house.myProperty.b);
+
+let house2 = {
+  propertyTwo: [
+    { aa: "this" },
+    { bb: "can" },
+    { cc: "get" },
+    { gg: "complicated" }
+  ]
+};
+
+console.log(house2.propertyTwo[3]);
+
+let carLot = [
+    { year: 2015, make: 'Toyota', model: 'avensis'},
+    { year: 2018, make: 'opel', model: 'corsa'},
+    { year: 2013, make: 'audi', model: 'a4'}
+
+];
+
+console.log(carLot);
